bf3f92abbb6ac2162b67bcc28d7c6abeb6beacfc
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-01-21 16:03:34 -0300

    feat: global styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        <ThemeProvider theme={defaultTheme} />
+        <GlobalStyle />
     </React.StrictMode>
 );
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,22 @@
+export const defaultTheme = {
+    white: "#FFF",
+    "white-background": "#FAFAFA",
+
+    "white-100": "##F3F2F2",
+    "white-200": "#EEEDED",
+    "white-300": "#E6E5E5",
+    "white-400": "#D7D5D5",
+
+    "yellow-100": "#C47F17",
+    "yellow-300": "#DBAC2C",
+    "yellow-700": "#F1E9C9",
+
+    "purple-100": "#8047F8",
+    "purple-500": "#EBE5F9",
+    "purple-700": "#4B2995",
+
+    "gray-400": "#8D8686",
+    "gray-600": "#574F4D",
+    "gray-700": "#403937",
+    "gray-800": "#272221",
+};
