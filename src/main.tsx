879b4e018ace25a4a0620134ffb2c4d34574bc86
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-01-23 20:55:57 -0300

    feat: structured routers

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "styled-components";
-import { GlobalStyle } from "./styles/global";
-import { defaultTheme } from "./styles/themes/default";
-import coffeImage from "./assets/Coffe/Americano.svg";
+import { App } from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <ThemeProvider theme={defaultTheme} />
-        <img src={coffeImage} alt="" />
-        <GlobalStyle />
+        <App />
     </React.StrictMode>
 );
