3443737b5c5195c29ff70f81af557fee2b76cca2
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-01-25 02:31:36 -0300

    feat: Home structure without style

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,38 @@
+import { ShoppingCart, Timer } from "phosphor-react";
+import Coffee from "phosphor-react/dist/icons/Coffee";
+import Package from "phosphor-react/dist/icons/Package";
+
 export function Home() {
     return (
         <div>
-            <p>Home</p>
+            <div>
+                <p>Encontre o café perfeito para qualquer hora do dia</p>
+                <span>
+                    Com o Coffee Delivery você recebe seu café onde estiver, a
+                    qualquer hora Com o Coffee Delivery você recebe seu café
+                    onde estiver, a qualquer hora
+                </span>
+                <div>
+                    <div>
+                        <ShoppingCart size={32} />
+                        <p>Compra simples e segura</p>
+                    </div>
+                    <div>
+                        <Package size={32} />
+                        <p>Embalagem mantém o café intacto</p>
+                    </div>
+                    <div>
+                        <Timer size={32} />
+                        <p>Entrega rápida e rastreada</p>
+                    </div>
+                    <div>
+                        <Coffee size={32} />
+                        <p>O café chega fresquinho até você</p>
+                    </div>
+                </div>
+            </div>
+
+            <div>img</div>
         </div>
     );
 }
