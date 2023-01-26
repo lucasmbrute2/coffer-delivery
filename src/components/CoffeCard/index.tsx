e0c6d1baa5bab42b483d1cafa83e65c06174cb1f
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-01-26 01:39:01 -0300

    feat: Implemented coffe context api

--- src/components/CoffeCard/index.tsx
+++ src/components/CoffeCard/index.tsx
@@ -1,48 +1,75 @@
 import { ShoppingCart } from "phosphor-react";
+import { useContext } from "react";
 import minusIcon from "../../assets/minus.svg";
 import plusIcon from "../../assets/plus.svg";
+import { Coffe, CoffeContext } from "../../contexts/CoffeContext";
+import { SvgImport } from "../SvgImport";
 import {
     CoffeCardPriceSection,
     CoffeCardPriceWrapper,
     CoffeCardQuantitySection,
     CoffeCardQuantityWrapper,
     CoffeCardWrapper,
-    CoffeImage,
     CoffeLabel,
     CoffeLabelContainer,
     Quantity,
 } from "./style";
 
 interface CoffeCardProps {
-    imgPath?: string;
+    coffe: Coffe;
 }
 
-export function CoffeCard({ imgPath }: CoffeCardProps) {
+export function CoffeCard({ coffe }: CoffeCardProps) {
+    const { coffes, setCoffes } = useContext(CoffeContext);
+    const { imgSrc, quantity, price, labels, description, name } = coffe;
+
+    //improve that
+    function handleMinusQuantity() {
+        setCoffes((prev) => {
+            return prev.map((co) => {
+                if (co === coffe && co.quantity > 1) {
+                    co.quantity -= 1;
+                }
+                return co;
+            });
+        });
+    }
+
+    function handlePlusQuantity() {
+        setCoffes((prev) => {
+            return prev.map((co) => {
+                if (co === coffe) {
+                    co.quantity += 1;
+                }
+                return co;
+            });
+        });
+    }
     return (
         <CoffeCardWrapper>
-            <CoffeImage src={imgPath} alt="" />
+            <SvgImport svgName={imgSrc} />
             <CoffeLabelContainer>
-                <CoffeLabel>especial</CoffeLabel>
-                <CoffeLabel>alcoolico</CoffeLabel>
-                <CoffeLabel>gelado</CoffeLabel>
+                {labels.map((label) => (
+                    <CoffeLabel>{label}</CoffeLabel>
+                ))}
             </CoffeLabelContainer>
 
-            <h3>Expresso tradicional</h3>
-            <span>O tradicional café feito com água quente e grãos moidos</span>
+            <h3>{name}</h3>
+            <span>{description}</span>
 
             <CoffeCardPriceWrapper>
                 <CoffeCardPriceSection>
                     <p>R$</p>
-                    <p>9,90</p>
+                    <p>{price}</p>
                 </CoffeCardPriceSection>
 
                 <CoffeCardQuantityWrapper>
                     <CoffeCardQuantitySection>
-                        <span>
+                        <span onClick={handleMinusQuantity}>
                             <img src={minusIcon} alt="" />
                         </span>
-                        <Quantity>1</Quantity>
-                        <span>
+                        <Quantity>{quantity}</Quantity>
+                        <span onClick={handlePlusQuantity}>
                             <img src={plusIcon} alt="" />
                         </span>
                     </CoffeCardQuantitySection>
---
+++ src/contexts/CoffeContext.tsx
@@ -0,0 +1,33 @@
+import { createContext, ReactNode, useState } from "react";
+import { svgKeyName } from "../components/SvgImport";
+
+export interface Coffe {
+    id: string;
+    imgSrc: svgKeyName;
+    name: string;
+    labels: string[];
+    description: string;
+    quantity: number;
+    price: number;
+}
+
+interface CoffeContextData {
+    coffes: Coffe[];
+    setCoffes: React.Dispatch<React.SetStateAction<Coffe[]>>;
+}
+
+export const CoffeContext = createContext({} as CoffeContextData);
+
+interface CoffeProviderrProps {
+    children: ReactNode;
+}
+
+export function CoffeProvider({ children }: CoffeProviderrProps) {
+    const [coffes, setCoffes] = useState<Coffe[]>([]);
+
+    return (
+        <CoffeContext.Provider value={{ coffes, setCoffes }}>
+            {children}
+        </CoffeContext.Provider>
+    );
+}
