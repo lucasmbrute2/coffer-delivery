ae8e2ce8eedaedfdfc3acaf8eaab337f63535b15
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-02-11 11:26:51 -0300

    feat: added edit and remove actions

--- src/components/reducers/cart/actions.ts
+++ src/components/reducers/cart/actions.ts
@@ -2,19 +2,28 @@ import { CoffeIntoCart } from "../../../contexts/CoffeContext";
 
 export enum ActionTypes {
     ADD_NEW_PRODUCT = "ADD_NEW_PRODUCT",
-    EDIT_ITEM = "EDIT_ITEM",
+    EDIT_ITEM_QUANTITY = "EDIT_ITEM_QUANTITY",
+    REMOVE_ITEM_FROM_CART = "REMOVE_ITEM_FROM_CART",
 }
 
-export function addProductIntoCart(item: CoffeIntoCart) {
+export function addProductIntoCart(item: CoffeIntoCart, quantity: number) {
     return {
         type: ActionTypes.ADD_NEW_PRODUCT,
         item,
+        quantity,
     };
 }
 
 export function editProductQuantityIntoCart(item: CoffeIntoCart) {
     return {
-        type: ActionTypes.EDIT_ITEM,
+        type: ActionTypes.EDIT_ITEM_QUANTITY,
         item,
+    };
+}
+
+export function removeItemFromCart(id: string) {
+    return {
+        type: ActionTypes.REMOVE_ITEM_FROM_CART,
+        id,
     };
 }
