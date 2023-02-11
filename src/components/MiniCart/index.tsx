ed8b2835249a94bf19b911eb87c656f2ff5d9359
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-02-11 11:28:21 -0300

    refactor: getting cart information from useReduce instead useState

--- src/components/MiniCart/index.tsx
+++ src/components/MiniCart/index.tsx
@@ -4,8 +4,8 @@ import { useContext } from "react";
 import { CoffeContext } from "../../contexts/CoffeContext";
 
 export function MiniCart() {
-    const { cart } = useContext(CoffeContext);
-    const miniCartQuantityOfItems = cart.reduce(
+    const { cartState } = useContext(CoffeContext);
+    const miniCartQuantityOfItems = cartState.reduce(
         (acc, curr) => (acc += curr.quantity),
         0
     );
